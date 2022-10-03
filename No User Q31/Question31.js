fca637ccd3f20bb1a608a8c35827be1c64b1c61c
MuhammadHamzaFarooq <mhamza2021999@gmail.com>
2022-10-04 01:26:47 +0500

    complete 31 questions

--- No User Q31/Question31.js
+++ No User Q31/Question31.js
@@ -3,3 +3,24 @@
 // • If the list is empty, print the message We need to find some users!
 
 // • Remove all of the usernames from your array, and make sure the correct message is printed.
+
+const users = ["Admin", "Ali", "Sufyan", "Irfan", "Ziyad"];
+
+if (users.length === 0) {
+    return console.log(" We need to find some users")
+}
+
+for (let i in users) {
+    if (users[i] === "Admin") {
+        console.log(`Hello ${users[i]}, would you like to see a status report?`)
+    }
+    else {
+        console.log(`Hello ${users[i]}, thank you for logging in again`)
+    }
+}
+
+// remove all users 
+for (let i in users) {
+    users.pop()
+}
+
