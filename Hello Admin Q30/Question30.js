3712f8fe4f20b924200cd9d9a0d38fdd115a05de
MuhammadHamzaFarooq <mhamza2021999@gmail.com>
2022-10-03 23:20:40 +0500

    complete 30 questions

--- Hello Admin Q30/Question30.js
+++ Hello Admin Q30/Question30.js
@@ -3,3 +3,15 @@
 // • If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
 
 // • Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.
+
+
+const users = ["Admin", "Ali", "Sufyan", "Irfan", "Ziyad"];
+
+for (let i in users) {
+    if (users[i] === "Admin") {
+        console.log(`Hello ${users[i]}, would you like to see a status report?`)
+    }
+    else {
+        console.log(`Hello ${users[i]}, thank you for logging in again`)
+    }
+}
